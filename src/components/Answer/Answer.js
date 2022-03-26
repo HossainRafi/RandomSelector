460ccbdf735390d8d25fa3c7e346eb8993adcd6c
HossainRafi <hossainrafi71@gmail.com>
2022-03-26 14:41:51 +0600

    comment

---
+++ src/components/Answer/Answer.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './Answer.css'
+const Answer = () => {
+  return (
+    <div>
+      <div className="qna">
+        <h2 className="question">How React Works?</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis
+          tempore ut, et laborum ad nulla officiis? Facere, consequuntur quam.
+          Dolorum excepturi, ea itaque suscipit consequatur, nulla quibusdam,
+          accusamus voluptatem enim iste facere quo totam omnis explicabo sit
+          corrupti. A, ad.
+        </p>
+      </div>
+      <div className="qna">
+        <h2 className="question">How useState Works ?</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis
+          tempore ut, et laborum ad nulla officiis? Facere, consequuntur quam.
+          Dolorum excepturi, ea itaque suscipit consequatur, nulla quibusdam,
+          accusamus voluptatem enim iste facere quo totam omnis explicabo sit
+          corrupti. A, ad.
+        </p>
+      </div>
+      <div className="qna">
+        <h2 className="question">How React Works?</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis
+          tempore ut, et laborum ad nulla officiis? Facere, consequuntur quam.
+          Dolorum excepturi, ea itaque suscipit consequatur, nulla quibusdam,
+          accusamus voluptatem enim iste facere quo totam omnis explicabo sit
+          corrupti. A, ad.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Answer;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,38 +1,51 @@
 import React, { useEffect, useState } from "react";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
+import Answer from "./../Answer/Answer";
 import "./Shop.css";
 
 const Shop = () => {
+  // ========== States ==========
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+
+  // ========== Load Data ==========
   useEffect(() => {
     fetch("fake-data.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  // ========== Cart Button Functionality ==========
   const addToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
   };
   return (
-    <div className="main-container">
-      <div className="card-container">
-        {products.map((product) => (
-          <Product key={product.id} data={product} addToCart={addToCart} />
-        ))}
-      </div>
-      <div className="cart-container">
-        <h3 className="text">Selected Items:</h3>
-        {cart.map((item) => (
-          <Cart item={item} />
-        ))}
+    <div>
+      <div className="main-container">
+        {/* ========== Product Card Section ========== */}
+        <div className="card-container">
+          {products.map((product) => (
+            <Product key={product.id} data={product} addToCart={addToCart} />
+          ))}
+        </div>
 
-        <div className="btn-group">
-          <button className="items-btn">Choose One</button>
-          <button className="items-btn">Remove All</button>
+        {/* ========== Product Cart Section ========== */}
+        <div className="cart-container">
+          <h3 className="text">Selected Items:</h3>
+          {cart.map((item) => (
+            <Cart item={item} />
+          ))}
+
+          {/* ========== Choose & Remove Buttons ========== */}
+          <div className="btn-group">
+            <button className="items-btn">Choose One</button>
+            <button className="items-btn">Remove All</button>
+          </div>
         </div>
       </div>
+      <Answer />
     </div>
   );
 };
