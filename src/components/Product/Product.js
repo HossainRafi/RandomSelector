9bf867210e71780a18c1babc789405648807e105
HossainRafi <hossainrafi71@gmail.com>
2022-03-26 08:56:30 +0600

    card done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,22 +4,17 @@ import "./Product.css";
 const Product = (props) => {
   const { image, name, price } = props.data;
   return (
-    <div className="card-container">
-      <div className="img">
-        <img src={image} alt="" />
+    <div className="product">
+      <img src={image} alt="" />
+      <div className="product-info">
+        <h3 className="product-name">{name}</h3>
+        <h3>Price: {price}</h3>
       </div>
-      <div className="info">
-        <h3>{name}</h3>
-        <h5>Price: {price}</h5>
-        <button className="cart-btn">
-          <h6>
-            Add to Cart
-            <span className="icon">
-              <i class="las la-shopping-cart"></i>
-            </span>
-          </h6>
-        </button>
-      </div>
+      <button className="btn-cart">
+        <h3>
+          Add to Cart <i className="btn-icon las la-shopping-cart"></i>
+        </h3>
+      </button>
     </div>
   );
 };
