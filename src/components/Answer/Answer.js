2081e79bc7bb59f1f2d1045efc9e04b610574ea1
HossainRafi <hossainrafi71@gmail.com>
2022-03-26 19:33:24 +0600

    complete

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,37 +1,33 @@
 import React from "react";
-import './Answer.css'
+import "./Answer.css";
 const Answer = () => {
   return (
     <div>
       <div className="qna">
         <h2 className="question">How React Works?</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis
-          tempore ut, et laborum ad nulla officiis? Facere, consequuntur quam.
-          Dolorum excepturi, ea itaque suscipit consequatur, nulla quibusdam,
-          accusamus voluptatem enim iste facere quo totam omnis explicabo sit
-          corrupti. A, ad.
-        </p>
+        <h4>
+          আমরা React এ যে Code গুলো লিখি React সেগুলোকে JavaScript এ Convert করে
+          ফেলে। React এ Virtual DOM থাকে। React আমাদের লিখা Code কে Virtual DOM
+          এ রুপান্তর করে ব্রাউজার এর dom tree এর সাথে compare করে দেখে যে
+          specific ঠিক কোন Elemnet টা Change করা হয়েছে। সেই Change অনুসারে
+          React পুরো DOM কে আবার re-render না করে শুধু যেই Element Change হয়েছে
+          সেখানেই Change করে।এতে Browser এর আর বাড়তি কাজ করা লাগেনা। যার ফলে
+          React এত দ্রুত Render হয়।
+        </h4>
       </div>
       <div className="qna">
         <h2 className="question">How useState Works ?</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis
-          tempore ut, et laborum ad nulla officiis? Facere, consequuntur quam.
-          Dolorum excepturi, ea itaque suscipit consequatur, nulla quibusdam,
-          accusamus voluptatem enim iste facere quo totam omnis explicabo sit
-          corrupti. A, ad.
-        </p>
-      </div>
-      <div className="qna">
-        <h2 className="question">How React Works?</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis
-          tempore ut, et laborum ad nulla officiis? Facere, consequuntur quam.
-          Dolorum excepturi, ea itaque suscipit consequatur, nulla quibusdam,
-          accusamus voluptatem enim iste facere quo totam omnis explicabo sit
-          corrupti. A, ad.
-        </p>
+        <h4>
+          PROPS: Props are read only. যার মানে Props কে Modify করা যায়না। Props
+          এর মান যেটা Declare করা হয় Props এর মান সেটাই থাকে। মূলত Props থেকে
+          Data পাঠানো হয়।
+        </h4>
+        <h4>
+          STATE: State এর মান Asynchronous ভাবে Change হয়। State এর মান
+          this.setState use করে Change করা যায়। React এর মধ্যে কোনো Value
+          Updated করার প্রয়োজন হতেপারে এমন Situation কে Handle করার জন্য State
+          Use করা হয়।
+        </h4>
       </div>
     </div>
   );
